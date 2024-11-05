09b4f688282dcca0be83a55624a34aa1d5214f02
Jeeva Ramasamy <jeevanandan@gmail.com>
2024-11-05 18:07:30 -0500

    Fix Issue: Training phase did not work

--- Colorization.py
+++ Colorization.py
@@ -1,19 +1,23 @@
-# %%
-from PIL import Image
-import numpy as np
-from skimage import color
-import torch
-import torch.nn.functional as F
-from IPython import embed
 import os
-# %%
-
+import time
+import copy
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision import transforms
+from PIL import Image
+from skimage import color
+from torchinfo import summary
 
 def load_img(img_path):
 	out_np = np.asarray(Image.open(img_path))
 	if(out_np.ndim==2):
 		out_np = np.tile(out_np[:,:,None],3)
 	return out_np
+
 def resize_img(img, HW=(150,150), resample=3):
 	return np.asarray(Image.fromarray(img).resize((HW[1],HW[0]), resample=resample))
 
@@ -48,14 +52,9 @@ def postprocess_tens(tens_orig_l, out_ab, mode='bilinear'):
 	out_lab_orig = torch.cat((tens_orig_l, out_ab_orig), dim=1)
 	return color.lab2rgb(out_lab_orig.data.cpu().numpy()[0,...].transpose((1,2,0)))
 
-# %%
-import torch
-from torch import nn
-
 class BaseColor(nn.Module):
 	def __init__(self):
 		super(BaseColor, self).__init__()
-
 		self.l_cent = 50.
 		self.l_norm = 100.
 		self.ab_norm = 110.
@@ -71,13 +70,6 @@ class BaseColor(nn.Module):
 
 	def unnormalize_ab(self, in_ab):
 		return in_ab*self.ab_norm
-
-# %%
-import torch
-import torch.nn as nn
-import numpy as np
-from IPython import embed
-
 
 class ECCVGenerator(BaseColor):
     def __init__(self, norm_layer=nn.BatchNorm2d):
@@ -141,7 +133,6 @@ class ECCVGenerator(BaseColor):
         model8+=[nn.ReLU(True),]
         model8+=[nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),]
         model8+=[nn.ReLU(True),]
-
         model8+=[nn.Conv2d(256, 313, kernel_size=1, stride=1, padding=0, bias=True),]
 
         self.model1 = nn.Sequential(*model1)
@@ -155,7 +146,7 @@ class ECCVGenerator(BaseColor):
 
         self.softmax = nn.Softmax(dim=1)
         self.model_out = nn.Conv2d(313, 2, kernel_size=1, padding=0, dilation=1, stride=1, bias=False)
-        self.upsample4 = nn.Upsample(scale_factor=4, mode='bilinear')
+        self.upsample4 = nn.Upsample(scale_factor=4, mode='bilinear', align_corners=True)
 
     def forward(self, input_l):
         conv1_2 = self.model1(self.normalize_l(input_l))
@@ -172,62 +163,13 @@ class ECCVGenerator(BaseColor):
 
 def eccv16(pretrained=True):
 	model = ECCVGenerator()
-	if(pretrained):
+	if pretrained:
 		import torch.utils.model_zoo as model_zoo
 		model.load_state_dict(model_zoo.load_url('https://colorizers.s3.us-east-2.amazonaws.com/colorization_release_v2-9b330a0b.pth',map_location='cpu',check_hash=True))
 	return model
 
-# %%
-
-import argparse
-import matplotlib.pyplot as plt
-import glob
-
-# # load colorizers
-# colorizer_eccv16 = eccv16(pretrained=False).eval()
-
-# # default size to process images is 256x256
-# # grab L channel in both original ("orig") and resized ("rs") resolutions
-# img = load_img('archive/landscape Images/color/52.jpg')
-# (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(150,150))
-
-
-# # colorizer outputs 256x256 ab map
-# # resize and concatenate to original L channel
-# img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
-# out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
-
-# %%
-# plt.figure(figsize=(12,8))
-# plt.subplot(2,2,1)
-# plt.imshow(img)
-# plt.title('Original')
-# plt.axis('off')
-
-# plt.subplot(2,2,2)
-# plt.imshow(img_bw)
-# plt.title('Input')
-# plt.axis('off')
-
-# plt.subplot(2,2,3)
-# plt.imshow(out_img_eccv16)
-# plt.title('Output (ECCV 16)')
-# plt.axis('off')
-# plt.show()
-# %%
 class ColorizationDataset(torch.utils.data.Dataset):
-    '''Used to train the entire model
-    def __init__(self, root_dir, transform=None):
-        import os
-        self.root_dir = root_dir
-        self.transform = transform
-        self.image_paths = []
-        for root, dirs, files in os.walk(root_dir):
-            for file in files:
-                if file.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp', '.tiff')):
-                    self.image_paths.append(os.path.join(root, file))
-	'''
-    #Testing 100 images fiirst
+    '''Used to train the entire model'''
     def __init__(self, root_dir, transform=None, max_samples=100):
         self.root_dir = root_dir
         self.transform = transform
@@ -247,48 +189,23 @@ class ColorizationDataset(torch.utils.data.Dataset):
             img = self.transform(img)
         img_lab = color.rgb2lab(img.numpy().transpose((1, 2, 0)))
         img_lab = (img_lab + [0, 128, 128]) / [100, 255, 255]
-        L = img_lab[..., 0:1]
+        L = img_lab[..., :1]
         ab = img_lab[..., 1:]
         L = torch.from_numpy(L.transpose((2, 0, 1))).float()
         ab = torch.from_numpy(ab.transpose((2, 0, 1))).float()
+        L = torch.nn.functional.pad(L, (1, 1, 1, 1), mode='constant', value=0)  # Padding (left, right, top, bottom)
+        ab = torch.nn.functional.pad(ab, (1, 1, 1, 1), mode='constant', value=0)  # Padding (left, right, top, bottom)
         return L, ab
 
+transform = transforms.Compose([transforms.Resize((150, 150)), transforms.ToTensor()])
 
-import time
-import copy
-import torchvision
-from torchvision import transforms
-from tqdm import tqdm
-
-transform = transforms.Compose([transforms.Resize((150, 150)),
-                                transforms.ToTensor()])
-# train_dataset = torchvision.datasets.ImageFolder(root='dataset', transform=transform)
-# val_dataset = torchvision.datasets.ImageFolder(root='dataset', transform=transform)
-
-#This would be for entire dataset if the rest of this shit works:
-#train_dataset = ColorizationDataset(root_dir='dataset', transform=transform)
-#val_dataset = ColorizationDataset(root_dir='dataset', transform=transform)
-
-# Use a smaller dataset for testing
-train_dataset = ColorizationDataset(root_dir='dataset', transform=transform, max_samples=100)
-val_dataset = ColorizationDataset(root_dir='dataset', transform=transform, max_samples=100)
-
+train_dataset = ColorizationDataset(root_dir='drive/MyDrive/dataset (1)', transform=transform)
+val_dataset = ColorizationDataset(root_dir='drive/MyDrive/dataset (1)', transform=transform)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=4, shuffle=True, num_workers=4)
 
 print("Train Dataset")
 print(len(train_dataset), len(val_dataset))
-
-
-# train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4)
-# val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=4, shuffle=True, num_workers=4)
-
-#Same as before this is for 7k
-# train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4)
-# val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=4, shuffle=True, num_workers=4)
-
-#Faster training for proof of concept since it is a smaller dataset / limited one:
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4)
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=4, shuffle=False, num_workers=4)
-
 print("Train Loader")
 print(len(train_loader), len(val_loader))
 
@@ -300,76 +217,6 @@ print("Dataset_sizes / Dataloaders")
 print(dataset_sizes)
 print(dataloaders)
 
-'''
-Prev:
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-    since = time.time()
-
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_loss = 1e10
-
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
-
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                model.train()  # Set model to training mode
-            else:
-                model.eval()   # Set model to evaluate mode
-
-            running_loss = 0.0
-			
-            print("Dataloaders[phase]:", dataloaders[phase])
-			# Iterate over data.
-            for labels, inputs in dataloaders[phase]:
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-                print("Inputs:" , inputs)
-				#labels = labels.to(device)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    loss = criterion(outputs, labels)
-
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-
-            if phase == 'train':
-                scheduler.step()
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-
-            print('{} Loss: {:.4f}'.format(
-                phase, epoch_loss))
-
-            # deep copy the model
-            if phase == 'val' and epoch_loss < best_loss:
-                best_loss = epoch_loss
-                best_model_wts = copy.deepcopy(model.state_dict())
-
-        print()
-
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val loss: {:4f}'.format(best_loss))
-
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model
-'''
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
 
@@ -393,49 +240,49 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                 model.train()
             else:
                 model.eval()
-        with tqdm(total=len(dataloaders), desc=f'{phase} Epoch {epoch+1}/{num_epochs}', unit='batch') as pbar:
-            for inputs, targets in dataloaders[phase]:
-                inputs = inputs.to(device)
-                targets = targets.to(device)
+            with tqdm(total=len(dataloaders), desc=f'{phase} Epoch {epoch+1}/{num_epochs}', unit='batch') as pbar:
+                for inputs, targets in dataloaders[phase]:
+                    inputs = inputs.to(device)
+                    targets = targets.to(device)
 
-                optimizer.zero_grad()
+                    optimizer.zero_grad()
 
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
 
-                    # Compute loss
-                    loss = criterion(outputs, targets)
+                        # Compute loss
+                        loss = criterion(outputs, targets)
 
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
 
-                running_loss += loss.item() * inputs.size(0)
+                    running_loss += loss.item() * inputs.size(0)
 
-                #Progress bar
-                pbar.set_postfix({'Loss': loss.item()})
-                pbar.update(1)
-            if phase == 'train':
-                scheduler.step()
+                    #Progress bar
+                    pbar.set_postfix({'Loss': loss.item()})
+                    pbar.update(1)
+                if phase == 'train':
+                    scheduler.step()
 
-            epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_loss = running_loss / dataset_sizes[phase]
+                print(phase, epoch_loss)
 
-            if phase == 'train':
-                train_losses.append(epoch_loss)
-            else:
-                val_losses.append(epoch_loss)
+                if phase == 'train':
+                    train_losses.append(epoch_loss)
+                else:
+                    val_losses.append(epoch_loss)
 
-            print(f'{phase} Loss: {epoch_loss:.4f}')
+                print(f'{phase} Loss: {epoch_loss:.4f}')
 
-            # Deep copy the model
-            if phase == 'val' and epoch_loss < best_loss:
-                best_loss = epoch_loss
-                best_model_wts = copy.deepcopy(model.state_dict())
-                torch.save(model.state_dict(), 'best_model.pth')
-        epoch_duration = time.time() - epoch_start_time
-        print(f'Epoch {epoch+1} completed in {epoch_duration:.2f} seconds')
-
-        print()
+                # Deep copy the model
+                if phase == 'val' and epoch_loss < best_loss:
+                    best_loss = epoch_loss
+                    best_model_wts = copy.deepcopy(model.state_dict())
+                    torch.save(model.state_dict(), 'best_model.pth')
+            epoch_duration = time.time() - epoch_start_time
+            print(f'Epoch {epoch+1} completed in {epoch_duration:.2f} seconds')
+            print()
 
     time_elapsed = time.time() - since
     print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
@@ -445,22 +292,19 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     model.load_state_dict(best_model_wts)
     return model, train_losses, val_losses
 
-from torchinfo import summary
-
-
 if __name__ == '__main__':
     model = eccv16(pretrained=False)
     model = model.to(device)
 
-    summary(model, input_size=(4, 1, 150, 150))  
-
+    summary(model, input_size=(4, 1, 150, 150))
 
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-    num_epochs = 25
+    num_epochs = 2
 
-    model, train_losses, val_losses = train_model(model, criterion, optimizer, scheduler, num_epochs=25)
+    model, train_losses, val_losses = train_model(model, criterion, optimizer, scheduler, num_epochs)
+    print(train_losses, val_losses)
 
     # Plot the loss curves
     epochs = range(1, num_epochs+1)
